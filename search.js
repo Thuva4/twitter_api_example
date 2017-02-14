7ac3a4094fa0dc4e82f85dbd8679e4bd38ebb3b5
thuva4 <tharma.thuva@gmail.com>
2017-02-14 15:28:01 +0530

    comment

--- search.js
+++ search.js
@@ -1,18 +1,20 @@
-console.log("Example is up now..")
+console.log("Server is running ... ")
+
 var Twit = require('twit');
 var config = require('./config');
 var T = new Twit(config);
+
 var params = {
-q: 'Google -RT',
-geocode:'7.8731,80.7718,100mi',
-lang: 'en',
-count: 100
+q: 'Google -RT', // query exclude "RT"
+// geocode:'7.8731,80.7718,100mi', // Geo location lat lon , radius
+// lang: 'en',
+count: 100 // number of tweets for the request
 }
 
 T.get('search/tweets', params,searchedData);
 
 function searchedData(err, data, response) {
   for(var i = 0; i < data.statuses.length ;i++){
-    console.log(data.statuses[i].text);
+    console.log(data.statuses[i].text); // Getting tweets'text from the response
   }
 }
